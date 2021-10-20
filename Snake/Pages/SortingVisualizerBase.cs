a2a06b669c770990def2f4ccdc1dfcafacb7a142
sibisaravanan94 <sibisaravanan94@gmail.com>
2021-10-20 23:21:15 +0530

    SortingVisualizer init

---
+++ Snake/Pages/SortingVisualizerBase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snake.Pages
+{
+    public class SortingVisualizerBase:ComponentBase
+    {
+        public const int NUMBER_OF_ARRAY_BARS = 260;
+        public const int ANIMATION_SPEED_MS = 1;
+        public const string PRIMARY_COLOR = "turquoise";
+        public const string SECONDARY_COLOR = "red";
+        public int[] array { get; set; }
+        protected override Task OnInitializedAsync()
+        {
+            resetArray();
+            return base.OnInitializedAsync();
+        }
+        public void resetArray()
+        {
+            array = new int[NUMBER_OF_ARRAY_BARS];
+            for (int i = 0; i < NUMBER_OF_ARRAY_BARS; i++)
+            {
+                array[i]= randomIntFromInterval(5, 545);
+            }
+        }
+        public int randomIntFromInterval(int start, int end)
+        {
+            Random rand = new Random();
+            int number = rand.Next(start, end + 1);
+            return number;
+        }
+    }
+}
