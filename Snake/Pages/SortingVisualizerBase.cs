05297a64d3c7430983bdb0496f00ec9766198787
sibisaravanan94 <sibisaravanan94@gmail.com>
2021-10-27 23:47:15 +0530

    Quick sort animation

--- Snake/Pages/SortingVisualizerBase.cs
+++ Snake/Pages/SortingVisualizerBase.cs
@@ -13,10 +13,12 @@ namespace Snake.Pages
         public const string PRIMARY_COLOR = "turquoise";
         public const string SECONDARY_COLOR = "red";
         public const string SORTING_COLOR = "blue";
+        public const string PIVOT_COLOR = "yellow";
         public List<int[]> animations { get; set; }
         public int[] array { get; set; }
         public string[] color { get; set; }
         System.Timers.Timer t;
+        public HashSet<int> sortIndex { get; set; }
         protected override Task OnInitializedAsync()
         {
             resetArray();
@@ -40,16 +42,41 @@ namespace Snake.Pages
         }
         public async void quickSort()
         {
+            animations = new List<int[]>();
             int[] cloneArray = (int[])array.Clone();
-            getMergeSortAnimations(cloneArray);
+            getQuickSortAnimations(cloneArray, 0, cloneArray.Length - 1, animations);
+            int i = 0;
+            t = new System.Timers.Timer();
+            t.Elapsed += async (s, e) =>
+            {
+                if (i < animations.Count)
+                {
+                    int[] temp = animations[i];
+                    bool isColorChange = temp[0] < 2;
+                    if (isColorChange)
+                    {
+                        color[temp[1]] = PIVOT_COLOR;
+                        color[temp[2]] = temp[0] == 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                        color[temp[3]] = temp[0] == 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                        //color[temp[3]] = i % 3 == 0 ? SORTING_COLOR : (temp[3] == 0) ? PRIMARY_COLOR : SORTING_COLOR;
+                    }
+                    else if(temp[0]==3)
+                    {
+                        color[temp[1]] = PRIMARY_COLOR;
+                    }
+                    else
+                    {
+                        array[temp[1]] = temp[2];
+                    }
+                }
+                i++;
+                await InvokeAsync(StateHasChanged);
+            };
+            t.Interval = 0.1;
+            t.Start();
         }
 
-        public static int[] getQuickSortAnimations(int[] array)
-        {
-            QuickSort(array, 0, array.Length - 1);
-            return array;
-        }
-        public static void QuickSort(int[] array, int startIdx, int endIdx)
+        public void getQuickSortAnimations(int[] array, int startIdx, int endIdx, List<int[]> animations)
         {
             if (startIdx >= endIdx)
                 return;
@@ -58,8 +85,12 @@ namespace Snake.Pages
             int rightIdx = endIdx;
             while (rightIdx >= leftIdx)
             {
+                animations.Add(new int[] { 0, pivotIdx, leftIdx, rightIdx });
+                animations.Add(new int[] { 1, pivotIdx, leftIdx, rightIdx });
                 if (array[leftIdx] > array[pivotIdx] && array[rightIdx] < array[pivotIdx])
                 {
+                    animations.Add(new int[] { 2,  leftIdx, array[rightIdx] });
+                    animations.Add(new int[] { 2, rightIdx, array[leftIdx] });
                     swap(leftIdx, rightIdx, array);
                 }
                 if (array[leftIdx] <= array[pivotIdx])
@@ -71,21 +102,25 @@ namespace Snake.Pages
                     rightIdx--;
                 }
             }
+            animations.Add(new int[] { 2, pivotIdx, array[rightIdx] });
+            animations.Add(new int[] { 2, rightIdx, array[pivotIdx] });
+            animations.Add(new int[] { 3, pivotIdx });
+            //sortIndex.Add(rightIdx);
             swap(pivotIdx, rightIdx, array);
             bool leftSubarrayIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1);
             if (leftSubarrayIsSmaller)
             {
-                QuickSort(array, startIdx, rightIdx - 1);
-                QuickSort(array, rightIdx + 1, endIdx);
+                getQuickSortAnimations(array, startIdx, rightIdx - 1, animations);
+                getQuickSortAnimations(array, rightIdx + 1, endIdx, animations);
             }
             else
             {
-                QuickSort(array, rightIdx + 1, endIdx);
-                QuickSort(array, startIdx, rightIdx - 1);
+                getQuickSortAnimations(array, rightIdx + 1, endIdx, animations);
+                getQuickSortAnimations(array, startIdx, rightIdx - 1, animations);
             }
 
         }
-        public static void swap(int i, int j, int[] array)
+        public void swap(int i, int j, int[] array)
         {
             int temp = array[j];
             array[j] = array[i];
